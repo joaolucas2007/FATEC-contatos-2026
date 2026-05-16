dfa4341986da8b693833ef654438a47973ae3f92
joaolucas2007 <joao.git2000p@gmail.com>
2026-05-15 21:35:55 -0300

    Subindo o form, para ao clicar no login, ir para o form de cadastro

--- js/app.js
+++ js/app.js
@@ -5,3 +5,39 @@ exibirContatos();
 
 import { uploadParaCloudinary } from "./cloudnary.js";
 uploadParaCloudinary();
+
+
+import { registrarContato, exibirContatos } from "./contatos.js";
+import { uploadParaCloudinary } from "./cloudnary.js";
+
+
+const btnLogin = document.getElementById("btn-login");
+const loginContainer = document.getElementById("login-container");
+const mainContent = document.getElementById("main-content");
+
+
+btnLogin.addEventListener("click", () => {
+
+  const usuario = document.getElementById("login-user").value;
+  const senha = document.getElementById("login-password").value;
+
+
+  if (usuario && senha) {
+
+
+    loginContainer.style.display = "none";
+
+
+    mainContent.style.display = "block";
+
+    registrarContato();
+    exibirContatos();
+    uploadParaCloudinary();
+
+  } else {
+
+    alert("Preencha usuário e senha");
+
+  }
+
+});
